546c6ececfb02c74d2be7fcb05df71996932a493
BinnyJ <binnyjohn39@gmail.com>
2019-11-28 23:54:49 -0600

    Java Questions

---
+++ src/countWord/countOptionTwo.java
@@ -0,0 +1,10 @@
+package countWord;
+
+public class countOptionTwo {
+    // write a java program that count word
+    //String str = { Java Programming question }
+    public static void main(String[] args) {
+        String str = "Java Programming question";
+        System.out.println(str.split(" ").length);
+    }
+}
